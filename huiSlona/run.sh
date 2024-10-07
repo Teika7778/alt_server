9da9dc4c23019eebf825a6f745ff9d59f47aebf7
Teika7778 <max2005swet@gmail.com>
2024-10-07 22:18:49 +0300

    NEW

--- huiSlona/run.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-# Forge requires a configured set of both JVM and program arguments.
-# Add custom JVM arguments to the user_jvm_args.txt
-# Add custom program arguments {such as nogui} to this file in the next line before the "$@" or
-#  pass them to this script directly
-java -Xms8G -Xmx8G @libraries/net/minecraftforge/forge/1.19.2-43.4.2/unix_args.txt "$@"
